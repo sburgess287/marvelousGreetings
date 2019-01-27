56523033d46f6e818958b068fe65e4b89735d2d1
Sarah Burgess <sarahburgess287@gmail.com>
2019-01-27 10:38:32 -0800

    Added app.js file to serve dynamic files on the index.html page.  Updated index.html to connect to app.js

---
+++ public/app.js
@@ -0,0 +1,11 @@
+// create mock data of json array
+// default card
+
+
+// create function that returns mock data (to verify update in browser)
+
+
+// function to display default card
+
+
+// function to get called on page load: display default card
