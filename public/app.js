8c26df58b9f12cfdde065f71646491f3fb9ead46
Sarah Burgess <sarahburgess287@gmail.com>
2019-01-29 23:09:56 -0800

    Updated link to html2canvas script in index.html, created function to take screenshot in app.js using html2canvas library

--- public/app.js
+++ public/app.js
@@ -33,12 +33,15 @@ function getDefaultCard(callbackFn) {
 // function to display default card
 function displayDefaultCard(data) {
   for (index in data.cardStructure) { // adapt for showing list of cards? currently only 1 item in array
-    $('body').append(
+    // $('body').append(
+      $('.contentContainer').append(
       '<p>' + data.cardStructure[index].headline + '</p>',
       '<p>' + data.cardStructure[index].bodyText+ '</p>',
       '<p>' + data.cardStructure[index].character + '</p>',
       `<img src="`+ data.cardStructure[index].image + `">`, 
-      '<p>"Data provided by Marvel. © 2014 Marvel"</p>'  // link to image, later
+      '<p>"Data provided by Marvel. © 2014 Marvel"</p>',  // link to image, later
+      '<button class="test">Click to screenshot</button>' // add option to ignore this css class
+
     )
   }
 }
@@ -48,7 +51,45 @@ function getAndDisplayCard() {
   getDefaultCard(displayDefaultCard);
 }
 
+// function to take screenshot using library
+// function takeScreenShot() {
+//   // html2canvas(document.querySelector(".contentContainer")).then(canvas => {
+//   //   document.body.appendChild(canvas)
+//   // });
+//     html2canvas(document.body).then(function(canvas) {
+//     // Export the canvas to its data URI representation
+//     var base64image = canvas.toDataURL("image/png");
+
+//     // Open the image in a new window
+//     window.open(base64image , "_blank");
+// });
+
+// }
+
+
 // function to get called on page load: display default card: BOOM!!!
 $(function() {
   getAndDisplayCard();
+  $('.contentContainer').on('click', '.test', event => {
+    //takeScreenShot();
+    console.log('button clicked');
+    // interesting because when using this function below with 
+    // allowTaint: true option set, error appears that tainted canvases cannot be exported
+    // html2canvas(document.body).then(function(canvas) {
+    //       // Export the canvas to its data URI representation
+    //       var base64image = canvas.toDataURL("image/png");
+      
+    //       // Open the image in a new window
+    //       window.open(base64image , "_blank");
+    //   });
+
+
+    let x = this.getElementById('screenshot-card');
+    html2canvas(x, { allowTaint: true}).then(canvas => {
+      document.body.appendChild(canvas);
+     
+    })
+  })
+
+  
 });
