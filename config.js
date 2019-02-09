71f627831d5a13c8fa3df8f2bf97705fa3e0b16e
Sarah Burgess <sarahburgess287@gmail.com>
2019-02-09 15:03:16 -0800

    Updated local db url to mongodb:localhost/marvelousgreetings

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 "use strict"
 
 exports.DATABASE_URL = 
-  process.env.DATABASE_URL || "mongodb://localhost/cardTestDb";
-  // process.env.DATABASE_URL || "mongodb://localhost/marvelousgreetings"; ??
+  process.env.DATABASE_URL || "mongodb://localhost/marvelousgreetings"
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || "mongodb://localhost/test-marvelousgreetings";
 exports.PORT = process.env.PORT || 8080;
