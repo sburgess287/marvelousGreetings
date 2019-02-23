7399c58a5f6fd566679fae12931a0646d48ff663
Sarah Burgess <sarahburgess287@gmail.com>
2019-02-23 12:25:37 -0800

    Updated auth/strategies.js with local and JWT strategy, uncommented expiration in config.js of JWT token now that strategy is in place

--- auth/strategies.js
+++ auth/strategies.js
@@ -9,5 +9,50 @@ const { User } = require('../users/models');
 const { JWT_SECRET } = require('../config');
 
 // error says local strategy is not defined because not written yet!
+const localStrategy = new LocalStrategy(function(username, password, callback) {
+  let user;
+  User.findOne({ username: username })
+    .then(_user => {
+      user = _user; 
+      if (!user) {
+        // return rejected promise to be handled in catch block
+        return Promise.reject({
+          reason: 'LoginError', 
+          message: 'Incorrect username or password'
+        })
+      }
+      // valid password
+      return user.validatePassword(password)
+    })
+    .then(isValid => {
+      if (!isValid) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect username or password'
+        })
+      }
+      // pass in user assigned to req in req.user
+      return callback(null, user)
+    })
+    .catch(err => {
+      if (err.reason === "LoginError") {
+        return callback(null, false, err)
+      }
+      return callback(err, false)
+    })
+})
+
+
+// JWT strategy
+const jwtStrategy = new JwtStrategy(
+  {
+    secretOrKey: JWT_SECRET,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+    algorithms: ['HS256']
+  },
+  function(payload, callback) {
+    callback(null, payload.user);
+  }
+)
 
 module.exports = { localStrategy, jwtStrategy };
